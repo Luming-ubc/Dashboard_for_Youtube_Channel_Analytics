6f26b219c326e31ca6b28bd6e783ae668ff58071
Luming21 <lumingyang.ut@gmail.com>
2022-09-05 16:23:34 -0700

    Tidy code

--- get_data.py
+++ get_data.py
@@ -6,7 +6,7 @@ import time
 
 # %%
 # function get_channel_summary
-def get_channel_summary(channel_id_input):
+def get_channel_summary(channel_id):
     """ This function summarize below info by youtube channel
         title, id, country, view_count, sub_count, video_count 
     Argument:
@@ -17,33 +17,24 @@ def get_channel_summary(channel_id_input):
         pandas DataFrame: a summary table of key info
     """
     
-    with open('youtube_api_key.txt', 'r') as f:
+    with open('youtube_api_key_2.txt', 'r') as f:
         API_KEY = f.read()
     
-    # 1. channels url
+    # use channels url
     url = "https://www.googleapis.com/youtube/v3/channels"
 
-    # 1.1 statistics + id (ChannelID
-    params = {'key': API_KEY, 'part': 'statistics', 'id': channel_id_input, 'maxResults': 50}
+    # access channel snippet, statistics, and brandingSettings
+    params = {'key': API_KEY, 'part': ['snippet', 'statistics', 'brandingSettings'], 'id': channel_id, 'maxResults': 50}
     response = requests.get(url, params=params)
     r_dict = response.json()
 
-    viewCount = r_dict['items'][0]['statistics']['viewCount']
-    subscriberCount = r_dict['items'][0]['statistics']['subscriberCount']
-    videoCount = r_dict['items'][0]['statistics']['videoCount']
+    # get view_count, subscriber_count, video_count, channel_title
+    view_count = r_dict['items'][0]['statistics']['viewCount']
+    subscriber_count = r_dict['items'][0]['statistics']['subscriberCount']
+    video_count = r_dict['items'][0]['statistics']['videoCount']
+    channel_title = r_dict['items'][0]['snippet']['title']
 
-    # 1.2 part:snippet + filter: id (channelID)
-    params = {'key': API_KEY, 'part': 'snippet', 'id': channel_id_input, 'maxResults': 50}
-    response = requests.get(url, params=params)
-    r_dict = response.json()
-
-    channelTitle = r_dict['items'][0]['snippet']['title']
-    
-    # 1.3 brandingSettings + ID
-    params = {'key': API_KEY, 'part': 'brandingSettings', 'id': channel_id_input, 'maxResults': 50}
-    response = requests.get(url, params=params)
-    r_dict = response.json()
-
+    # get brandingSettings country
     if 'country' in r_dict['items'][0]['brandingSettings']['channel'].keys():
         country = r_dict['items'][0]['brandingSettings']['channel']['country']
     else:
@@ -51,20 +42,20 @@ def get_channel_summary(channel_id_input):
     
     # fomualte a dataframe
     channel_summary={
-            'channel_title': [channelTitle],
-            'channel_id':[channel_id_input], 
+            'channel_title': [channel_title],
+            'channel_id':[channel_id], 
             'country': [country],
-            'view_count': [viewCount], 
-            'subscriber_count': [subscriberCount], 
-            'video_count': [videoCount]
+            'view_count': [view_count], 
+            'subscriber_count': [subscriber_count], 
+            'video_count': [video_count]
             }
     channel_summary = pd.DataFrame(data=channel_summary)
     return channel_summary
 
-def main():
 
-    # Get channel summary
-    channel_summary = get_channel_summary('UCCKlp1JI9Yg3-cUjKPdD3mw')
-    channel_summary.to_csv('data/channel_summary.csv')
-
+# Get channel summary
+channel_id = 'UCCKlp1JI9Yg3-cUjKPdD3mw'  # Magic Ingredient
+channel_summary = get_channel_summary(channel_id)
+channel_summary
+channel_summary.to_csv('data/channel_summary.csv')
 
