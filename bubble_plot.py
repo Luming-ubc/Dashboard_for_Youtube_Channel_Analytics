c0ec0c37c03d1cc58b6a35111506eea4558d5dbd
Luming21 <lumingyang.ut@gmail.com>
2022-09-06 09:58:00 -0700

    Add bubble plot

--- bubble_plot.py
+++ bubble_plot.py
@@ -1,40 +1,19 @@
 # %%
 from turtle import color
 import altair as alt
-from vega_datasets import data
+from time import time, sleep
 
-source = data.gapminder_health_income.url
-
-alt.Chart(source).mark_circle().encode(
-    alt.X('income:Q', scale=alt.Scale(type='log')),
-    alt.Y('health:Q', scale=alt.Scale(zero=False)),
-    size='population:Q'
-)
 # %%
 import pandas as pd
-df = pd.read_csv('data/all_videos_daoyueshe.csv')
-
-df[['minutes']] = df['duration'].str.extract(r'PT(\d+)M', expand=True).astype('int')
-df[['seconds']] = df['duration'].str.extract(r'M(\d+)S', expand=True).fillna(0).astype('int')
-df['total_seconds'] = 60*df['minutes'] + df['seconds']
-df
+df = pd.read_csv('data/processed.csv')
 
 # %%
-alt.Chart(df).transform_timeunit(
-    Year='year(publish_time)',
-    Month = 'month(publish_time)'
-).mark_circle().encode(
-    alt.X('like_count:Q'),
-    alt.Y('view_count:Q'),
-    size='total_seconds:Q',
-    color='total_seconds:Q'
+alt.Chart(df).mark_circle().encode(
+    alt.Y('value:Q', scale=alt.Scale(type="log")),
+    alt.X('yearmonth(publish_time):O'),
+    size='duration(s):Q',
+    opacity='duration(s):Q',
+    color='variable'
 )
-# ).facet(
-#     row=alt.Column(
-#         'Year:T',
-#         title=None,
-#         header=alt.Header(labelAngle=0, labelAlign='left', format='%Y')
-#     )
-# )
 
-# %%
+
