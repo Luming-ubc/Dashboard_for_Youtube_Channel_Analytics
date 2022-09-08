d5fd5cd6b56af490fa308b50068867a4d04cdf16
Luming21 <lumingyang.ut@gmail.com>
2022-09-07 17:46:35 -0700

    Update preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,17 +1,25 @@
 # %%
 import pandas as pd
 import altair as alt
-df = pd.read_csv('data/all_videos_daoyueshe.csv')
+df = pd.read_csv('data/all_videos_map.csv')
 
-df[['minutes']] = df['duration'].str.extract(r'PT(\d+)M', expand=True).astype('int')
+df[['minutes']] = df['duration'].str.extract(r'PT(\d+)M', expand=True).fillna(0).astype('int')
+
 df[['seconds']] = df['duration'].str.extract(r'M(\d+)S', expand=True).fillna(0).astype('int')
 df['duration(s)'] = 60 * df['minutes'] + df['seconds']
 
 
 df['publish_time'] = pd.to_datetime(df['publish_time'])
-df = df.melt(id_vars=['video_id', 'publish_time', 'duration(s)'], value_vars=['view_count', 'like_count', 'comment_count'])
+df['year'] = df['publish_time'].dt.year
 df.to_csv('data/processed.csv')
-
+df
 # %%
+
+df = df.melt(id_vars=['video_id', 'video_title', 'publish_time', 'duration(s)', 'year'], value_vars=['view_count', 'like_count', 'comment_count'])
+df.to_csv('data/processed_long.csv')
+df
+
+
+
 
 # %%
